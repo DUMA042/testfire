ac17aa72a501a1b6237c5e5472f8811258e797c7
DUMA042 <dumebiikeoffiah@gmail.com>
2022-03-23 15:40:08 +0200

    Not fully working Giving classLoader error message

--- app/src/main/java/com/example/testfire/MainActivity.kt
+++ app/src/main/java/com/example/testfire/MainActivity.kt
@@ -87,7 +87,7 @@ class MainActivity : ComponentActivity() {
 fun getGoogleSignInClient(context: Context): GoogleSignInClient {
     val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
 //         Request id token if you intend to verify google user from your backend server
-     .requestIdToken(context.getString(R.string.ID_token))
+     .requestIdToken(context.getString(R.string.ID_GToken))
         .requestEmail()
         .build()
 
